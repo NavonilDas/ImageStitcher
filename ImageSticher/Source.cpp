151a4bbd38a4fff9f3c4616cd69631bc8357e51d
NavonilDas <navneeladas@gmail.com>
2018-09-10 11:45:09 +0530

    Added a function to calculate Homograpy Matrix

--- ImageSticher/Source.cpp
+++ ImageSticher/Source.cpp
@@ -4,6 +4,17 @@
 #include <iostream>
 
 
+int calcHomographyMat(cv::Mat img1, cv::Mat img2,cv::Mat &result) {
+	cv::Mat gray1;
+	cv::Mat gray2;
+	cv::cvtColor(img1, gray1, cv::COLOR_BGR2GRAY);
+	cv::cvtColor(img2, gray2, cv::COLOR_BGR2GRAY);
+	if(!gray1.data || !gray2.data) {
+
+	}
+	return 0; // Sucessfully Calculated
+}
+
 int main(int argc, char** argv)
 {
 	cv::Mat result;
@@ -11,16 +22,16 @@ int main(int argc, char** argv)
 	std::vector< cv::Mat > imgs = std::vector< cv::Mat >();
 	cv::String dir = "E:\\InputImages";
 	cv::glob(dir, filenames);
+	int64 start = 0, end = 0;
 
 	for (int i = 0; i < filenames.size(); i++) {
 		cv::Mat src = cv::imread(filenames[i], cv::IMREAD_COLOR);
-		if (src.data) {
+		//if (src.data) {
 			imgs.push_back(src);
-		}
+		// }
 	}
-	cv::Stitcher stitcher = cv::Stitcher::createDefault();
-	unsigned long start = 0, end = 0;
-
+	cv::Stitcher stitcher = cv::Stitcher::createDefault(true);
+	
 	start = cv::getTickCount();
 	cv::Stitcher::Status status = stitcher.stitch(imgs, result);
 	end = cv::getTickCount();
